4f4480d3d7ae23f58bfb5dc6af9759caaa68bdf1
Arnav Balpande <arnavbalpande14@gmail.com>
2023-05-02 10:54:11 +0530

    madlibs

---
+++ madlibs.py
@@ -0,0 +1,6 @@
+noun= input("Enter Name\n")
+adjective1=input("Adjective 1\n")
+adjective2=input("Adjective 2\n")
+verb=input("Verb\n")
+madlib=f"Hi this is {noun} and i am a very {adjective1} and {adjective2} person. I like {verb}."
+print(madlib)
